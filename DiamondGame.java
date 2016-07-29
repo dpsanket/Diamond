593ac10ba30767e18d5c817ec49fa188df3d443c
ayushiagrawal <Ayushi.Agrawal@cdk.com>
2016-07-29 15:18:03 +0530

    Add files via upload

---
+++ DiamondGame.java
@@ -0,0 +1,11 @@
+/**
+ * Created by agrawaay on 7/29/2016.
+ */
+public class DiamondGame {
+    public static void main(String args[]) {
+
+    }
+    public class suit {
+        int cards[13] = {2,3,4,5,6,7,8,9,10,11,12,13,14};
+    }
+}
